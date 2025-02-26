479e30f485155a3d21cb9dbe769cd560637453ff
mohamed <me.mo.75.20.09.biomy@gmail.com>
2025-02-26 16:07:50 +0200

    media queries was added

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+
 let buttons = document.querySelectorAll(".btn")
+let submitButton = document.querySelector("input[type='submit']")
 let numOfStars = 0;
+let firstCard = document.querySelector("div.first-case")
+let submitCard = document.querySelector("div.submit-case")
+let spanOfStars = document.querySelector("span.stars-num")
+let warning = document.querySelector(".warning")
 
 buttons.forEach((ele) => {
   ele.onclick = function (event) {
@@ -8,7 +14,24 @@ buttons.forEach((ele) => {
     })
     this.classList.add("selected")
     numOfStars = this.dataset.num
-    console.log(numOfStars)
     event.preventDefault()
   }
 })
+
+submitButton.onclick = function (event) {
+  if (numOfStars !== 0) {
+    firstCard.classList.add("none")
+    spanOfStars.append(numOfStars)
+    submitCard.classList.remove("none")
+  } else {
+    warning.style.visibility = "visible"
+    // removeInVisibility(warning);
+  }
+  event.preventDefault()
+}
+
+let removeInVisibility = function (ele) {
+  setTimeout(() => {
+    ele.style.visibility = "hidden"
+  }, 1500);
+}
